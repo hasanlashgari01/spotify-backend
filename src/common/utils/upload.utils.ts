0022bd787d3b09a7066f51e1515ec25ca791d383
Hasan Lashgari <hasanlashgari01@gmail.com>
2025-09-08 22:37:50 +0330

    change upload size

--- src/common/utils/upload.utils.ts
+++ src/common/utils/upload.utils.ts
@@ -1,21 +1,18 @@
-import {
-    FileTypeValidator,
-    HttpStatus,
-    MaxFileSizeValidator,
-    ParseFilePipe,
-    ParseFilePipeBuilder,
-} from "@nestjs/common";
+import { FileTypeValidator, MaxFileSizeValidator, ParseFilePipe } from "@nestjs/common";
+
+const audio_size = process.env.AUDIO_SIZE || 10;
+const image_size = process.env.IMAGE_SIZE || 1;
 
 export const ImageValidation = new ParseFilePipe({
     validators: [
-        new MaxFileSizeValidator({ maxSize: 1 * 1024 * 1024 }),
+        new MaxFileSizeValidator({ maxSize: image_size * 1024 * 1024 }),
         new FileTypeValidator({ fileType: "image/(png|jpg|jpeg|webp)" }),
     ],
 });
 
 export const OptionalImageValidation = new ParseFilePipe({
     validators: [
-        new MaxFileSizeValidator({ maxSize: 1 * 1024 * 1024 }),
+        new MaxFileSizeValidator({ maxSize: image_size * 1024 * 1024 }),
         new FileTypeValidator({ fileType: "image/(png|jpg|jpeg|webp)" }),
     ],
     fileIsRequired: false,
@@ -23,7 +20,7 @@ export const OptionalImageValidation = new ParseFilePipe({
 
 export const AudioValidation = new ParseFilePipe({
     validators: [
-        new MaxFileSizeValidator({ maxSize: 8 * 1024 * 1024 }),
+        new MaxFileSizeValidator({ maxSize: audio_size * 1024 * 1024 }),
         new FileTypeValidator({ fileType: "audio/(mpeg|wav|ogg|webm|aac)" }),
     ],
     fileIsRequired: false,
