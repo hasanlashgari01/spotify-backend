978aa1a32846e40242a9c8b008c6315f2b413b6d
Hasan Lashgari <hasanlashgari01@gmail.com>
2025-10-26 20:28:40 +0330

    get 10 popular songs

--- src/modules/artist/artist.module.ts
+++ src/modules/artist/artist.module.ts
@@ -1,14 +1,14 @@
-import {Module} from "@nestjs/common";
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {AuthModule} from "../auth/auth.module";
-import {UserEntity} from "../users/entities/user.entity";
-import {ArtistController} from "./artist.controller";
-import {ArtistService} from "./artist.service";
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthModule } from "../auth/auth.module";
+import { UserEntity } from "../users/entities/user.entity";
+import { ArtistController } from "./artist.controller";
+import { ArtistService } from "./artist.service";
+import { SongEntity } from "../song/entities/song.entity";
 
 @Module({
-    imports: [AuthModule, TypeOrmModule.forFeature([UserEntity])],
+    imports: [AuthModule, TypeOrmModule.forFeature([UserEntity, SongEntity])],
     controllers: [ArtistController],
     providers: [ArtistService],
 })
-export class ArtistModule {
-}
+export class ArtistModule {}
--- src/modules/artist/artist.service.ts
+++ src/modules/artist/artist.service.ts
@@ -1,20 +1,23 @@
-import {Injectable, NotFoundException} from "@nestjs/common";
-import {InjectRepository} from "@nestjs/typeorm";
-import {NotFoundMessage} from "src/common/enum/message.enum";
-import {Role} from "src/modules/users/enum/user.enum";
-import {Repository} from "typeorm";
-import {UserEntity} from "../users/entities/user.entity";
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { NotFoundMessage } from "src/common/enum/message.enum";
+import { SongEntity } from "src/modules/song/entities/song.entity";
+import { Role } from "src/modules/users/enum/user.enum";
+import { Repository } from "typeorm";
+import { UserEntity } from "../users/entities/user.entity";
 
 @Injectable()
 export class ArtistService {
     constructor(
         @InjectRepository(UserEntity)
-        private userRepository: Repository<UserEntity>
+        private userRepository: Repository<UserEntity>,
+        @InjectRepository(SongEntity)
+        private songRepository: Repository<SongEntity>,
     ) {}
 
     async getProfile(username: string) {
         const artist = await this.userRepository.findOne({
-            where: {username, role: Role.ARTIST},
+            where: { username, role: Role.ARTIST },
             select: {
                 id: true,
                 username: true,
@@ -28,5 +31,16 @@ export class ArtistService {
         if (!artist) throw new NotFoundException(NotFoundMessage.Artist);
 
         return artist;
+    }
+
+    async myPopularSongs(id: number) {
+        const isArtist = await this.userRepository.findOneBy({ id, role: Role.ARTIST });
+        if (!isArtist) throw new NotFoundException(NotFoundMessage.Artist);
+
+        return this.songRepository.find({
+            where: { artistId: id },
+            order: { plays: "DESC" },
+            take: 10,
+        });
     }
 }
