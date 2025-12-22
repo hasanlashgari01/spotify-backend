ca0cc57aa7d94b24937243106799575ad59b0fe5
Hasan Lashgari <hasanlashgari01@gmail.com>
2025-12-23 00:40:20 +0330

    add following raw type

---
+++ src/modules/follow/types/raw.type.ts
@@ -0,0 +1,8 @@
+export interface FollowingRaw {
+    following_id: string;
+    following_fullName: string;
+    following_username: string;
+    following_avatar: string | null;
+    following_role: string;
+    isFollowed: string;
+}
